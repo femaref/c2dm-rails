49d73081cd201b147477cbc20aaea20629b9c63c
Femaref <femaref@googlemail.com>
2012-01-16 11:46:47 +0100

    using Rails::Engine

--- lib/c2dm-rails.rb
+++ lib/c2dm-rails.rb
@@ -1,4 +1,4 @@
-require 'rails'
+require 'c2dm-rails'
 
 module C2dm
   mattr_accessor :auth_url
@@ -6,6 +6,16 @@ module C2dm
   mattr_accessor :push_url
   
   mattr_accessor :default_source
+  
+  
+  def self.setup
+    yield self
+  end
+  
+  class Engine < Rails::Engine
+    config.c2dm = C2dm
+    
+    initializer "c2dm.initialize" do |app|
+      app.config.c2dm = C2dm
+    end
 end
-
-require 'c2dm/c2dm-rails'
