ffd2605513f35cdc2ca0fd4a03f72f112ed2a8a9
Femaref <femaref@googlemail.com>
2012-03-07 12:07:10 +0100

    typo in merge

--- app/models/c2dm/device.rb
+++ app/models/c2dm/device.rb
@@ -1,5 +1,5 @@
 class C2dm::Device < C2dm::Base  
-  sself.table_name= "c2dm_devices"
+  self.table_name= "c2dm_devices"
   
   belongs_to :app, :class_name => "C2dm::App"
   has_many :notifications, :class_name => "C2dm::Notification", :dependent => :destroy
